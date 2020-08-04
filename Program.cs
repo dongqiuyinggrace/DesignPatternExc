3092b15b27e6d849a7e737ed79ebdf2219617156
Grace Dong <dongqiuyinggrace@gmail.com>
2020-08-04 22:01:27 +1000

    organizeFiles

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace DesignPatternExc
         static void Main(string[] args)
         {
             
+
         }
     }
 }
--- src/Memento/EditorState.cs
+++ src/Memento/EditorState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DesignPatternDemos
+namespace DesignPatternExc
 {
     class EditorState
     {
